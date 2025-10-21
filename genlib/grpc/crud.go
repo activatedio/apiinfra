935dafa08265ff0fd2721cd00678308eccc29cba
Ben Tomasini <Ben.Tomasini@authwise.com>
2025-10-21 11:27:11 -0700

    updated crud generation

--- genlib/grpc/crud.go
+++ genlib/grpc/crud.go
@@ -2,10 +2,16 @@ package grpc
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/activatedio/protogen/proto"
 	"github.com/activatedio/protogen/tfl"
+	"github.com/gertd/go-pluralize"
 	"github.com/iancoleman/strcase"
+)
+
+var (
+	pl = pluralize.NewClient()
 )
 
 // CrudParams contains the options to build the crud service
@@ -15,6 +21,28 @@ type CrudParams struct {
 	ServiceTarget  proto.Service
 	// ParentPath is the api path to prefix in front of the paths for options
 	ParentPath string
+	// APIBasePath is the path to prepend to all http paths
+	APIBasePath string
+}
+
+// GetAPIMessageName returns the name of the message in snake_case format derived from the Message.GetName() result.
+func (c CrudParams) GetAPIMessageName() string {
+	return strcase.ToSnake(c.Message.GetName())
+}
+
+// GetPluralAPIMessageName returns the pluralized form of the API message name for the current CrudParams instance.
+func (c CrudParams) GetPluralAPIMessageName() string {
+	return pl.Plural(c.GetAPIMessageName())
+}
+
+// GetQualifiedMessageName returns the fully qualified name of the proto.Message by combining its package and message name.
+func (c CrudParams) GetQualifiedMessageName() string {
+	return GetQualifiedName(c.Message)
+}
+
+// GetNormalizedParentPath returns the ParentPath string without leading or trailing slashes.
+func (c CrudParams) GetNormalizedParentPath() string {
+	return strings.TrimSuffix(strings.TrimPrefix(c.ParentPath, "/"), "/")
 }
 
 // BuildCrud add the methods needed ot a service for a crud message
@@ -26,32 +54,42 @@ func BuildCrud(params CrudParams) {
 	mFile := params.MessagesTarget
 
 	svc.AddMethods(
-		proto.NewMethod(fmt.Sprintf("Get%sFoo", name), proto.MethodParams{
+		proto.NewMethod(fmt.Sprintf("Get%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("Get%sRequest", name),
 			ResponseName: fmt.Sprintf("Get%sResponse", name),
 		}).AddOptions(
-			proto.NewOption("google.api.http", proto.NewMessageValueConstant(tfl.NewMessageValue())),
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newGetOptions(params))),
 		),
 		proto.NewMethod(fmt.Sprintf("List%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("List%sRequest", name),
 			ResponseName: fmt.Sprintf("List%sResponse", name),
-		}),
+		}).AddOptions(
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newListOptions(params))),
+		),
 		proto.NewMethod(fmt.Sprintf("Create%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("Create%sRequest", name),
 			ResponseName: fmt.Sprintf("Create%sResponse", name),
-		}),
+		}).AddOptions(
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newCreateOptions(params))),
+		),
 		proto.NewMethod(fmt.Sprintf("Update%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("Update%sRequest", name),
 			ResponseName: fmt.Sprintf("Update%sResponse", name),
-		}),
+		}).AddOptions(
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newUpdateOptions(params))),
+		),
 		proto.NewMethod(fmt.Sprintf("Patch%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("Patch%sRequest", name),
 			ResponseName: fmt.Sprintf("Patch%sResponse", name),
-		}),
+		}).AddOptions(
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newPatchOptions(params))),
+		),
 		proto.NewMethod(fmt.Sprintf("Delete%s", name), proto.MethodParams{
 			RequestName:  fmt.Sprintf("Delete%sRequest", name),
 			ResponseName: fmt.Sprintf("Delete%sResponse", name),
-		}),
+		}).AddOptions(
+			proto.NewOption("google.api.http", proto.NewMessageValueConstant(newDeleteOptions(params))),
+		),
 	)
 
 	mFile.AddImports(
@@ -60,28 +98,108 @@ func BuildCrud(params CrudParams) {
 	)
 
 	mFile.AddMessages(
-		NewGetRequest(msg),
+		NewGetRequest(params),
 	)
 	mFile.AddMessages(
-		NewListRequestResponse(msg).Messages()...,
+		NewListRequestResponse(params).Messages()...,
 	)
 	mFile.AddMessages(
-		NewCreateRequest(msg),
+		NewCreateRequest(params),
 	)
 	mFile.AddMessages(
-		NewUpdateRequest(msg),
+		NewUpdateRequest(params),
 	)
 	mFile.AddMessages(
-		NewPatchRequest(msg),
+		NewPatchRequest(params),
 	)
 	mFile.AddMessages(
-		NewDeleteRequest(msg),
+		NewDeleteRequest(params),
 	)
 }
 
+func newGetOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("get", namePath(params)),
+	)
+}
+
+func newDeleteOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("delete", namePath(params)),
+	)
+}
+
+func newListOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("get", parentPath(params)+"/*"),
+	)
+}
+
+func newCreateOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("post", parentPath(params)+"/*"),
+		tfl.NewStringField("body", "*"),
+	)
+}
+
+func newUpdateOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("put", namePath(params)),
+		tfl.NewStringField("body", "*"),
+	)
+}
+
+func newPatchOptions(params CrudParams) tfl.MessageValue {
+
+	return tfl.NewMessageValue().AddFields(
+		tfl.NewStringField("patch", namePath(params)),
+		tfl.NewStringField("body", "*"),
+	)
+}
+
+func namePath(params CrudParams) string {
+
+	sb := strings.Builder{}
+
+	sb.WriteString(params.APIBasePath)
+
+	npp := params.GetNormalizedParentPath()
+	if npp != "" {
+		npp += "/"
+	}
+
+	sb.WriteString(fmt.Sprintf("/{name=%s%s/*}", npp, params.GetPluralAPIMessageName()))
+
+	return sb.String()
+
+}
+
+func parentPath(params CrudParams) string {
+
+	sb := strings.Builder{}
+
+	sb.WriteString(params.APIBasePath)
+
+	npp := params.GetNormalizedParentPath()
+
+	if npp == "" {
+		sb.WriteString(fmt.Sprintf("/%s", params.GetPluralAPIMessageName()))
+	} else {
+		sb.WriteString(fmt.Sprintf("/{parent=%s}/%s", npp, params.GetPluralAPIMessageName()))
+	}
+
+	return sb.String()
+
+}
+
 // NewGetRequest returns a RequestResponse with Request and Response messages derived from the provided proto message.
-func NewGetRequest(msg proto.Message) proto.Message {
-	return proto.NewMessage(fmt.Sprintf("Get%sRequest", msg.GetName())).AddFields(
+func NewGetRequest(params CrudParams) proto.Message {
+	return proto.NewMessage(fmt.Sprintf("Get%sRequest", params.Message.GetName())).AddFields(
 		proto.NewField("name", proto.FieldParams{
 			FieldType: "string",
 			Number:    1,
@@ -95,9 +213,9 @@ func NewGetRequest(msg proto.Message) proto.Message {
 }
 
 // NewListRequestResponse creates a RequestResponse with a List request and response message derived from the given proto.Message.
-func NewListRequestResponse(msg proto.Message) RequestResponse {
+func NewListRequestResponse(params CrudParams) RequestResponse {
 	return RequestResponse{
-		Request: proto.NewMessage(fmt.Sprintf("List%sRequest", msg.GetName())).AddFields(
+		Request: proto.NewMessage(fmt.Sprintf("List%sRequest", params.Message.GetName())).AddFields(
 			proto.NewField("parent", proto.FieldParams{
 				FieldType: "string",
 				Number:    1,
@@ -120,9 +238,9 @@ func NewListRequestResponse(msg proto.Message) RequestResponse {
 				Number:    5,
 			}),
 		),
-		Response: proto.NewMessage(fmt.Sprintf("List%sResponse", msg.GetName())).AddFields(
+		Response: proto.NewMessage(fmt.Sprintf("List%sResponse", params.Message.GetName())).AddFields(
 			proto.NewField("list", proto.FieldParams{
-				FieldType: GetQualifiedName(msg),
+				FieldType: params.GetQualifiedMessageName(),
 				Repeated:  true,
 				Number:    1,
 			}),
@@ -135,42 +253,42 @@ func NewListRequestResponse(msg proto.Message) RequestResponse {
 }
 
 // NewCreateRequest initializes a RequestResponse with formatted create request and response messages.
-func NewCreateRequest(msg proto.Message) proto.Message {
-	return proto.NewMessage(fmt.Sprintf("Create%sRequest", msg.GetName())).AddFields(
+func NewCreateRequest(params CrudParams) proto.Message {
+	return proto.NewMessage(fmt.Sprintf("Create%sRequest", params.Message.GetName())).AddFields(
 		proto.NewField("parent", proto.FieldParams{
 			FieldType: "string",
 			Number:    1,
 		}),
-		proto.NewField(strcase.ToLowerCamel(msg.GetName()), proto.FieldParams{
-			FieldType: GetQualifiedName(msg),
+		proto.NewField(params.GetAPIMessageName(), proto.FieldParams{
+			FieldType: params.GetQualifiedMessageName(),
 			Number:    2,
 		}),
 	)
 }
 
 // NewUpdateRequest creates a new RequestResponse with an Update request and response based on the given message name.
-func NewUpdateRequest(msg proto.Message) proto.Message {
-	return proto.NewMessage(fmt.Sprintf("Update%sRequest", msg.GetName())).AddFields(
+func NewUpdateRequest(params CrudParams) proto.Message {
+	return proto.NewMessage(fmt.Sprintf("Update%sRequest", params.Message.GetName())).AddFields(
 		proto.NewField("parent", proto.FieldParams{
 			FieldType: "name",
 			Number:    1,
 		}),
-		proto.NewField(strcase.ToLowerCamel(msg.GetName()), proto.FieldParams{
-			FieldType: GetQualifiedName(msg),
+		proto.NewField(params.GetAPIMessageName(), proto.FieldParams{
+			FieldType: params.GetQualifiedMessageName(),
 			Number:    2,
 		}),
 	)
 }
 
 // NewPatchRequest creates a RequestResponse with a "Patch" request and response message based on the given proto message.
-func NewPatchRequest(msg proto.Message) proto.Message {
-	return proto.NewMessage(fmt.Sprintf("Patch%sRequest", msg.GetName())).AddFields(
+func NewPatchRequest(params CrudParams) proto.Message {
+	return proto.NewMessage(fmt.Sprintf("Patch%sRequest", params.Message.GetName())).AddFields(
 		proto.NewField("parent", proto.FieldParams{
 			FieldType: "name",
 			Number:    1,
 		}),
-		proto.NewField(strcase.ToLowerCamel(msg.GetName()), proto.FieldParams{
-			FieldType: GetQualifiedName(msg),
+		proto.NewField(params.GetAPIMessageName(), proto.FieldParams{
+			FieldType: params.GetQualifiedMessageName(),
 			Number:    2,
 		}),
 		proto.NewField("field_mask", proto.FieldParams{
@@ -181,8 +299,8 @@ func NewPatchRequest(msg proto.Message) proto.Message {
 }
 
 // NewDeleteRequest creates a RequestResponse struct for delete operations based on the provided proto.Message.
-func NewDeleteRequest(msg proto.Message) proto.Message {
-	return proto.NewMessage(fmt.Sprintf("Delete%sRequest", msg.GetName())).AddFields(
+func NewDeleteRequest(params CrudParams) proto.Message {
+	return proto.NewMessage(fmt.Sprintf("Delete%sRequest", params.Message.GetName())).AddFields(
 		proto.NewField("parent", proto.FieldParams{
 			FieldType: "name",
 			Number:    1,
--- genlib/grpc/crud_test.go
+++ genlib/grpc/crud_test.go
@@ -30,19 +30,38 @@ func TestBuildCrud(t *testing.T) {
 				ServiceTarget:  proto.NewService("UnitService"),
 			},
 			expectedService: `service UnitService {
-  rpc GetUnitFoo (GetUnitRequest) returns (GetUnitResponse) {
+  rpc GetUnit (GetUnitRequest) returns (GetUnitResponse) {
     option (google.api.http) = {
+      get: "/{name=units/*}"
     };
   }
   rpc ListUnit (ListUnitRequest) returns (ListUnitResponse) {
+    option (google.api.http) = {
+      get: "/units/*"
+    };
   }
   rpc CreateUnit (CreateUnitRequest) returns (CreateUnitResponse) {
+    option (google.api.http) = {
+      post: "/units/*"
+      body: "*"
+    };
   }
   rpc UpdateUnit (UpdateUnitRequest) returns (UpdateUnitResponse) {
+    option (google.api.http) = {
+      put: "/{name=units/*}"
+      body: "*"
+    };
   }
   rpc PatchUnit (PatchUnitRequest) returns (PatchUnitResponse) {
+    option (google.api.http) = {
+      patch: "/{name=units/*}"
+      body: "*"
+    };
   }
   rpc DeleteUnit (DeleteUnitRequest) returns (DeleteUnitResponse) {
+    option (google.api.http) = {
+      delete: "/{name=units/*}"
+    };
   }
 }
 
@@ -89,6 +108,99 @@ message PatchUnitRequest {
 }
 
 message DeleteUnitRequest {
+  name parent = 1;
+}
+
+`,
+		},
+		{
+			name: "full",
+			params: grpc.CrudParams{
+				Message:        proto.NewMessage("ModifiedUnit").SetPackageName("modified_unit.api"),
+				MessagesTarget: proto.NewFile("modified_unit"),
+				ServiceTarget:  proto.NewService("ModifiedUnitService"),
+				ParentPath:     "/tenants/*/",
+				APIBasePath:    "/api/v2",
+			},
+			expectedService: `service ModifiedUnitService {
+  rpc GetModifiedUnit (GetModifiedUnitRequest) returns (GetModifiedUnitResponse) {
+    option (google.api.http) = {
+      get: "/api/v2/{name=tenants/*/modified_units/*}"
+    };
+  }
+  rpc ListModifiedUnit (ListModifiedUnitRequest) returns (ListModifiedUnitResponse) {
+    option (google.api.http) = {
+      get: "/api/v2/{parent=tenants/*}/modified_units/*"
+    };
+  }
+  rpc CreateModifiedUnit (CreateModifiedUnitRequest) returns (CreateModifiedUnitResponse) {
+    option (google.api.http) = {
+      post: "/api/v2/{parent=tenants/*}/modified_units/*"
+      body: "*"
+    };
+  }
+  rpc UpdateModifiedUnit (UpdateModifiedUnitRequest) returns (UpdateModifiedUnitResponse) {
+    option (google.api.http) = {
+      put: "/api/v2/{name=tenants/*/modified_units/*}"
+      body: "*"
+    };
+  }
+  rpc PatchModifiedUnit (PatchModifiedUnitRequest) returns (PatchModifiedUnitResponse) {
+    option (google.api.http) = {
+      patch: "/api/v2/{name=tenants/*/modified_units/*}"
+      body: "*"
+    };
+  }
+  rpc DeleteModifiedUnit (DeleteModifiedUnitRequest) returns (DeleteModifiedUnitResponse) {
+    option (google.api.http) = {
+      delete: "/api/v2/{name=tenants/*/modified_units/*}"
+    };
+  }
+}
+
+`,
+			expectedMessages: `syntax = "proto3";
+
+package modified_unit;
+
+import "google/protobuf/field_mask.proto";
+import "google/protobuf/empty.proto";
+
+message GetModifiedUnitRequest {
+  string name = 1;
+  repeated string fields = 2;
+}
+
+message ListModifiedUnitRequest {
+  string parent = 1;
+  repeated string fields = 2;
+  int32 page_size = 3;
+  string page_token = 4;
+  string selection = 5;
+}
+
+message ListModifiedUnitResponse {
+  repeated modified_unit.api.ModifiedUnit list = 1;
+  string next_page_token = 2;
+}
+
+message CreateModifiedUnitRequest {
+  string parent = 1;
+  modified_unit.api.ModifiedUnit modified_unit = 2;
+}
+
+message UpdateModifiedUnitRequest {
+  name parent = 1;
+  modified_unit.api.ModifiedUnit modified_unit = 2;
+}
+
+message PatchModifiedUnitRequest {
+  name parent = 1;
+  modified_unit.api.ModifiedUnit modified_unit = 2;
+  google.protobuf.FieldMask field_mask = 3;
+}
+
+message DeleteModifiedUnitRequest {
   name parent = 1;
 }
 
